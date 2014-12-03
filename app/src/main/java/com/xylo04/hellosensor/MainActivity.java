5b1ea99d2999a82fbfd5d08e86498e9106c49046
Chris Keller <xylo04@gmail.com>
2014-12-02 21:27:35 -0700

    Splitting off TapService WIP

--- app/src/main/java/com/xylo04/hellosensor/MainActivity.java
+++ app/src/main/java/com/xylo04/hellosensor/MainActivity.java
@@ -2,6 +2,7 @@ package com.xylo04.hellosensor;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,40 +12,44 @@ import android.os.Vibrator;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private SensorManager sensorManager;
-
-    private Vibrator vibrator;
-
-    private ScheduledExecutorService scheduledExecutorService;
-
     private TextView statusText;
+    private Button startServiceButton;
+    private Button stopServiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         statusText = (TextView) findViewById(R.id.status_text);
+        startServiceButton = (Button) findViewById(R.id.start_service_button);
+        stopServiceButton = (Button) findViewById(R.id.stop_service_button);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) == null) {
-            Log.w(TAG, "There is no accelerometer on this device");
-        }
-        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        if (!vibrator.hasVibrator()) {
-            Log.w(TAG, "There is no vibrator on this device");
-        }
-
-        scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.startService(new Intent(MainActivity.this, TapService.class));
+                statusText.setText("Service started");
+            }
+        });
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.stopService(new Intent(MainActivity.this, TapService.class));
+                statusText.setText("Service stopped");
+            }
+        });
     }
 
     @Override
@@ -54,69 +59,11 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        registerAccelerometer();
-    }
-
-    private void registerAccelerometer() {
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        if (accelerometer != null) {
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
-            Log.d(TAG, "Registered accelerometer");
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        float xAccel = sensorEvent.values[0];
-        float yAccel = sensorEvent.values[1];
-        float zAccel = sensorEvent.values[2];
-        double magnitude = Math
-                .sqrt(Math.pow(xAccel, 2) + Math.pow(yAccel, 2) + Math.pow(zAccel, 2));
-        statusText.setText(String.format(
-                "x: %f\ny:%f\nz: %f\n|a| %f",
-                xAccel,
-                yAccel,
-                zAccel,
-                magnitude));
-        if (magnitude > 3.5) {
-            Log.d(TAG, String.format("Tap! Mag %f", magnitude));
-            unregisterAccelerometer();
-            if (vibrator.hasVibrator()) {
-                vibrator.vibrate(100L);
-            }
-            scheduledExecutorService.schedule(new Runnable() {
-                @Override
-                public void run() {
-                    registerAccelerometer();
-                }
-            }, 200, TimeUnit.MILLISECONDS);
-        }
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterAccelerometer();
-    }
-
-    private void unregisterAccelerometer() {
-        sensorManager.unregisterListener(this);
-        Log.d(TAG, "Unregistered accelerometer");
     }
 }
---
+++ app/src/main/java/com/xylo04/hellosensor/TapService.java
@@ -0,0 +1,89 @@
+package com.xylo04.hellosensor;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.Vibrator;
+import android.util.Log;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TapService extends Service  implements SensorEventListener {
+
+    private static final String TAG = TapService.class.getSimpleName();
+
+    private SensorManager sensorManager;
+
+    private Vibrator vibrator;
+
+    private ScheduledExecutorService scheduledExecutorService;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) == null) {
+            Log.w(TAG, "There is no accelerometer on this device");
+        }
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (!vibrator.hasVibrator()) {
+            Log.w(TAG, "There is no vibrator on this device");
+        }
+
+        scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        registerAccelerometer();
+    }
+
+    private void registerAccelerometer() {
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+            Log.d(TAG, "Registered accelerometer");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float xAccel = sensorEvent.values[0];
+        float yAccel = sensorEvent.values[1];
+        float zAccel = sensorEvent.values[2];
+        double magnitude = Math
+                .sqrt(Math.pow(xAccel, 2) + Math.pow(yAccel, 2) + Math.pow(zAccel, 2));
+        if (magnitude > 3.5) {
+            Log.d(TAG, String.format("Tap! Mag %f", magnitude));
+            unregisterAccelerometer();
+            if (vibrator.hasVibrator()) {
+                vibrator.vibrate(100L);
+            }
+            scheduledExecutorService.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    registerAccelerometer();
+                }
+            }, 200, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private void unregisterAccelerometer() {
+        sensorManager.unregisterListener(this);
+        Log.d(TAG, "Unregistered accelerometer");
+    }
+
+}
